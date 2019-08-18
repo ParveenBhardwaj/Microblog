74f2d0eb191f27faf96119d37ff095fbd64de13a
parveen <parveen-bhardwaj@outlook.com>
2019-08-18 20:18:14 +1000

    Added Live translation to messages.

--- app/models.py
+++ app/models.py
@@ -76,12 +76,12 @@ class User(UserMixin, db.Model):
         return User.query.get(id)
 
 
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
 
     def __repr__(self):
         return f'<Post {self.body}>'
